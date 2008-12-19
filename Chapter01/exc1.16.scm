5ae59bbe000ef0d4007eaddce29e8b0a9650ec55
shinysky1986 <shinysky1986@496dd354-cd01-11dd-a2b5-e5f827957e07>
2008-12-19 10:52:30 +0000

    exercise 1.14-1.17

---
+++ Chapter01/exc1.16.scm
@@ -0,0 +1,37 @@
+#lang scheme
+;; Exercise 1.16
+
+(define (square x) (* x x))
+(define (even? x) (= (remainder x 2) 0))
+
+;; b^n = (b^2)^(n/2)
+(define (faster-expt b n)
+  (fe-iter 1 b n))
+(define (fe-iter a b n)
+  (cond ((= n 0) a)
+        ((or (= a 0) (= b 0)) 0)
+        ((even? n) (fe-iter a (square b) (/ n 2)))
+        (else (fe-iter (* a b) (square b) (/ (- n 1) 2)))))
+
+;; b^n = (b^(n/2))^2
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+;; test case
+(define (test b n)
+  (cond ((< n 0) #t)
+        ((= (faster-expt b n) (fast-expt b n)) (test b (- n 1)))
+        (else #f)))
+
+;; test suite
+(define (test235 n)
+  (and (test 2 n) (test 3 n) (test 5 n)))
+(define (ts list n)
+  (map (lambda (b) (test b n)) list))
+;; Why? Why?? Why?!
+;: (define (ts2 list n)
+;:  (apply and (map (lambda (b) (test b n)) list)))
+  
+
