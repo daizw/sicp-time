502189febbe5ca7d248bab3a30ef8cd936fe01f6
shinysky1986 <shinysky1986@496dd354-cd01-11dd-a2b5-e5f827957e07>
2009-01-06 10:33:01 +0000

    Exercise 1.23-1.27

--- trunk/Chapter01/exc1.23.scm
+++ trunk/Chapter01/exc1.23.scm
@@ -1,3 +1,5 @@
+;; Exercise 1.23
+
 #lang scheme
 (require (lib "19.ss" "srfi"))
 
@@ -29,12 +31,15 @@
 ;; with "searching for divisors" algorithm.
 (define (prime? n)
   (define (square x) (* x x))
+  (define (next div)
+    (cond ((= div 2) 3)
+          (else (+ div 2))))
   (define (smallest-divisor n)
     (find-divisor n 2))
   (define (find-divisor n test-divisor)
     (cond ((> (square test-divisor) n) n)
           ((divides? test-divisor n) test-divisor)
-          (else (find-divisor n (+ test-divisor 1)))))
+          (else (find-divisor n (next test-divisor)))))
   (define (divides? a b)
     (= (remainder b a) 0))
   (= n (smallest-divisor n)))
@@ -87,3 +92,27 @@
           ((prime? num) (test-iter num (- count 1) start-time))
           (else (test-iter num (- count 1) start-time))))
   (test-iter (search-for-primes start-num 1) times-count (runtime)))
+
+;; 测试结果不太稳定,
+;; 开始时:
+;; > (test 100000000 1000)
+;; 
+;; 100000001
+;; 100000003
+;; 100000005
+;; 100000007 *** 0s 150000ns
+;;  ****** DONE ****** 
+;; Test report:
+;; Tested number 100000007 for 1000 times, elapsed time:  *** 3s 999690000ns
+;; #(struct:tm:time time-duration 999690000 3)
+;;  start time: #(struct:tm:time time-utc 8070000 1231227615)
+;;  end time: #(struct:tm:time time-utc 7760000 1231227619)#t
+;; 
+;; 而使用Exercise 1.22的程序跑出来大约是6s, 也就是说新版本快了大约 1/3.
+;; 但是过一段时间后再测, 发现新版本快了约 1/2, 有时甚至更多.
+;; 理论上讲, 新版本快大约 1/3 的结果是比较靠谱的, 因为新版本虽然少判断了一半的除数, 但是也增加了"过程调用"和判断是否等于2的开销.
+;; 应该和测试使用的数字也有关系.
+;; 使用(test 10000 100000)测试, 发现结果如下:
+;; 新版本: 4s 470000ns
+;; 旧版本: 7s 780000ns
+;; 减少了大约40%的运行时间.
---
+++ trunk/Chapter01/exc1.27.scm
@@ -0,0 +1,24 @@
+;; Exercise 1.27
+
+#lang scheme
+
+(define (fermat-full-test n)
+  (define (square x) (* x x))
+  (define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp)
+           (remainder (square (expmod base (/ exp 2) m))
+                      m))
+          (else
+           (remainder (* base (expmod base (- exp 1) m))
+                      m))))
+  (define (try-it a)
+      (= (expmod a n n) a))
+  
+  (define (ft-iter a)
+    (cond ((>= a n) #t)
+          ((try-it a) (ft-iter (+ a 1)))
+          (else #f)))
+  
+  (ft-iter 2))
+
